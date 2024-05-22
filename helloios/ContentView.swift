7a8e1d6ac976429926af840890e94b3d72ddcb11
mrreiaz <reiazbubt@gmail.com>
2024-05-22 21:48:26 +0900

    Initial Commit

---
+++ helloios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloios
+//
+//  Created by Abdul Halim Reiaz on 2024/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloios/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  helloios
+//
+//  Created by Abdul Halim Reiaz on 2024/05/22.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
